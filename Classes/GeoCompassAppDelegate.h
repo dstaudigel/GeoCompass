8acb2748c3c3c0feb24ace925edc9f8c82c189fd
Daniel Staudigel <dstaudigel@theokorigroup.com>
2009-12-30 10:52:26 -0800

    Let's get started

---
+++ Classes/GeoCompassAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  GeoCompassAppDelegate.h
+//  GeoCompass
+//
+//  Created by Daniel Staudigel on 12/30/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+@interface GeoCompassAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
